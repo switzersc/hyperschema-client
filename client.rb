322431f4f6882491385ec5c2df609845c5453dc7
Shelby Switzer <shelbyswitzer@gmail.com>
2018-04-20 12:00:15 -0400

    Get tests passing and add source to gemfile so we can actually get stuff

--- client.rb
+++ client.rb
@@ -1,13 +1,23 @@
+require 'json'
+require 'uri_template'
+
 class Client
   attr_reader :json_doc, :json_hyperschema
 
   def initialize(json_doc, json_hyperschema)
-    @json_doc = json_doc
-    @json_hyperschema = json_hyperschema
+    @json_doc = JSON.parse json_doc
+    @json_hyperschema = JSON.parse json_hyperschema
   end
 
   def get_links
-    hash = JSON.parse json_doc
-    []
+    links.map do |link|
+      template = URITemplate.new(link["href"])
+      link["href"] = template.expand(json_doc)
+      link
+    end
+  end
+
+  def links
+    json_hyperschema["links"] || []
   end
 end
--- client_spec.rb
+++ client_spec.rb
@@ -1,20 +1,41 @@
 require 'rspec'
+require 'webmock/rspec'
 require_relative 'client.rb'
 
 describe Client do
 
-  context "with an empty doc and empty hyperschema" do
-    it "returns an empty list" do
-      client = Client.new("{}", "{}")
+  describe "#get_links" do
+    context "with an empty doc and empty hyperschema" do
+      it "returns an empty list" do
+        client = Client.new("{}", "{}")
 
-      expect(client.get_links).to eq([])
+        expect(client.get_links).to eq([])
+      end
+    end
+
+    context "with a non-empty schema and doc" do
+      let!(:schema) { File.read("example_hyperschema.json") }
+      let!(:doc) { File.read("example_json_doc.json") }
+
+      it "returns list of links" do
+        client = Client.new(doc, schema)
+
+        expect(client.get_links.size).to eq(2)
+      end
+
+      it "returns the LDO as is if there are no template variables" do
+        client = Client.new(doc, schema)
+
+        expect(client.get_links).to include({"rel" => "author","href" => "https://api.example.com/author"})
+      end
+
+      it "replaces variables in the URI template" do
+        client = Client.new(doc, schema)
+
+        expect(client.get_links).to include({"rel" => "self","href" => "https://api.example.com/things/1"})
+      end
     end
   end
 
-  context "with a real example!" do
-    it "returns list of links" do
-
-    end
-  end
-
+  describe ""
 end
